cb148c55a2eb38268b0922b9ed681deacc7e9c01
Iamneo <techadmin@iamneo.ai>
2023-09-21 05:12:23 +0000

    Backup Commit

---
+++ public/app.js
@@ -0,0 +1,18 @@
+function calculate()
+{
+    var price = parseInt(document.getElementById("loanAmount").value);
+    var duprate = parseInt(document.getElementById("interestRate").value);
+    var rate = duprate/12/100;    
+    var term = parseInt(document.getElementById("loanTerm").value);
+    var pow = Math.pow((1+rate),term)    
+    var sol = ((price*rate*pow)/(pow-1))
+    document.getElementById("ans").innerHTML = "EMI$:"+sol.toFixed(2);
+    if(sol>1000)
+    {
+      document.getElementById("ans").style.color = "red";
+    }
+    else
+    {
+      document.getElementById("ans").style.color = "green";
+    }
+}
